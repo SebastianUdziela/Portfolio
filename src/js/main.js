49501d44633733f46aa2432241132dac7c453198
Sebastian Udziela <drumer1001@gmail.com>
2020-03-28 13:08:40 +0100

    navigation button improvement

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable eqeqeq */
 const homeSlider = document.querySelector('.home_slider');
-const btn = document.querySelector('.nav_button');
-const btnClose = document.querySelector('.btn_close');
+const btn = document.querySelector('.nav_btn');
+const header = document.querySelector('header');
 const navLinks = document.querySelectorAll('.navigation li');
 let changeOpacity = 0;
-let flag = false;
 
 // Slider
 
@@ -33,36 +32,30 @@ const changeSliderTextName = () => {
     } else {
       clearInterval(Fadeout);
       setTimeout(() => {
-      changeOpacity = 0;
-      homeSlider.firstElementChild.style.opacity = changeOpacity;
-      homeSlider.lastElementChild.style.opacity = changeOpacity;
+        changeOpacity = 0;
+        homeSlider.firstElementChild.style.opacity = changeOpacity;
+        homeSlider.lastElementChild.style.opacity = changeOpacity;
         changeSliderProfesion();
       }, 2000);
     }
   }, 50);
 };
 
-
 setTimeout(changeSliderTextName, 1000);
 
 // Menu
 
 const handleNavBtn = () => {
-  if (!flag) {
-    flag = true;
-    document.querySelector('.navigation').classList.add('nav_toggle');
-  } else {
-    document.querySelector('.navigation').classList.remove('nav_toggle');
-    flag = false;
-  }
+  console.log('dd');
+  header.classList.toggle('nav_toggle');
 };
+
 navLinks.forEach((element) => {
   element.addEventListener('click', () => {
-    document.querySelector('.navigation').classList.remove('nav_toggle');
-    flag = false;
+    header.classList.remove('nav_toggle');
   });
 });
 
 
-btnClose.addEventListener('click', handleNavBtn);
+// btnClose.addEventListener('click', handleNavBtn);
 btn.addEventListener('click', handleNavBtn);
